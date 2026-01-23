f98f66edafd86f3dcf2b5b3c33c35fc63a872638
Maffile-Enky <2309832453@qq.com>
2026-01-23 21:29:32 +0800

    初始化sp

---
+++ src/main/java/maffile/co_tiktok/CoTiktokApplication.java
@@ -0,0 +1,13 @@
+package maffile.co_tiktok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoTiktokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoTiktokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maffile/co_tiktok/CoTiktokApplicationTests.java
@@ -0,0 +1,13 @@
+package maffile.co_tiktok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoTiktokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
